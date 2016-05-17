7fac23d85e56f53eac1cf2ce78413dbf537542c3
FreeSlave <freeslave93@gmail.com>
2016-05-18 00:06:17 +0300

    Try one script to test on travis

--- start_doveralls.sh
+++
@@ -1,3 +0,0 @@
-if [ $USE_DOVERALLS = "true" ]; then
-    ./doveralls
-fi
--- travis-script.sh
+++ travis-script.sh
@@ -1,4 +1,10 @@
+#!/bin/sh
+
 if [ $USE_DOVERALLS = "true" ]; then
     wget -O doveralls "https://github.com/ColdenCullen/doveralls/releases/download/v1.2.0/doveralls_linux_travis"
     chmod +x doveralls
+    dub test -b unittest-cov --compiler=${DC}
+    ./doveralls
+else
+    dub test --compiler=${DC}
 fi
